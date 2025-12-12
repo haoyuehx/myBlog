16e98c72ddd29cedf0b4d36fae5923223c16a243
今夕何夕 <luhaotian3936848@gmail.com>
2025-12-12 10:17:21 +0800

    update icon

--- static/js/dark.js
+++ static/js/dark.js
@@ -1,10 +1,10 @@
 var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
 
-toggle.addEventListener("click", () => {    
-    if (toggle.className === "btn fas fa-moon") {
+toggle.addEventListener("click", () => {
+    if (toggle.className === "btn fa-solid fa-moon") {
         setTheme("dark");
-    } else if (toggle.className === "btn fas fa-sun") {
+    } else if (toggle.className === "btn fa-solid fa-sun") {
         setTheme("light");
     }
 });
@@ -13,9 +13,9 @@ function setTheme(mode) {
     localStorage.setItem("dark-mode-storage", mode);
     if (mode === "dark") {
         darkTheme.disabled = false;
-        toggle.className = "btn fas fa-sun";
+        toggle.className = "btn fa-solid fa-sun";
     } else if (mode === "light") {
         darkTheme.disabled = true;
-        toggle.className = "btn fas fa-moon";
+        toggle.className = "btn fa-solid fa-moon";
     }
 }
